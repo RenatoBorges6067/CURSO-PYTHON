5842b64360b4add2af67b7d3675fa2704c2de200
Renato Borges <118218980+RenatoBorges6067@users.noreply.github.com>
2022-12-14 12:15:41 -0300

    Create AULA 7 desafio 7.py

---
+++ AULA 7 desafio 7.py
@@ -0,0 +1,6 @@
+print("DESCUBRA QUAL A AREA DA SUA PAREDE")
+a = float(input("ALTURA:"))
+l = float(input("LARGURA"))
+ar = a * l
+print("VOCE TEM UMA AREA DE {} METROS²".format(ar))
+print("VOCE PRECISARÁ DE {} LITROS DE TINTA".format(ar/2))

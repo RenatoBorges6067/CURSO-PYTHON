de62d0af732a8496bd1fcbda1ee7c9873f675fb5
Renato Borges <118218980+RenatoBorges6067@users.noreply.github.com>
2022-12-14 12:13:40 -0300

    Create AULA 7 desafio 3.py

---
+++ AULA 7 desafio 3.py
@@ -0,0 +1,6 @@
+print("ESCREVAS SUAS NOTAS!")
+n1 = float(input("1° NOTA:"))
+n2 = float(input("2° NOTA:"))
+t = n1+n2
+print("A SOMA DA SUA NOTA FOI {}".format(t))
+print("Á SUA MÉDIA FOI {}".format(t/2))

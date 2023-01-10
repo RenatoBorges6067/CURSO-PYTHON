8ff6f9ea08cd0b5a80d77549fbf4af7edaa18266
Renato Borges <118218980+RenatoBorges6067@users.noreply.github.com>
2023-01-10 17:04:33 -0300

    Update AULA 7 desafio 4.py

--- AULA 7 desafio 4.py
+++ AULA 7 desafio 4.py
@@ -1,1 +1,13 @@
-NADA
+medida = float(input("ESCREVA UMA MEDIDA"))
+KM = medida / 1000
+HM = medida / 100
+DAM = medida / 10
+DM = medida * 10
+CM = medida * 100
+MM = medida *1000
+print('A medida de M {} é igual a KM {}'.format(medida, KM,))
+print('A medida de M {} é igual a HM {}'.format(medida, HM))
+print('A medida de M {} é igual a DAM {}'.format(medida, DAM))
+print('A medida de M {} é igual a DM {}'.format(medida, DM))
+print('A medida de M {} é igual a CM {}'.format(medida, CM))
+print('A medida de M {} é igual a MM {}'.format(medida, MM))

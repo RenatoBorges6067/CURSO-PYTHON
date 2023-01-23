d0c43990843c6996e103429bfea6d3e5d836f840
Renato Borges <118218980+RenatoBorges6067@users.noreply.github.com>
2023-01-23 17:47:52 -0300

    Create AULA 9 desafio 24.py

---
+++ AULA 9 desafio 24.py
@@ -0,0 +1,5 @@
+cidade = str(input('ESCREVA O NOME DA SUA CIDADE:')).strip()
+print(cidade[:5].upper() == 'SANTO')
+
+#cid = str(input('Em que cidade voce nasceu? ')).strip()
+#print(cid[:5].upper() == 'SANTO')
